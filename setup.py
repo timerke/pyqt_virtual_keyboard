9c6bd4bbf04d4d4be3b7fa9dd34fc3dd8312ef0f
timerke <timerke@mail.ru>
2024-07-19 17:38:29 +0300

    Added setuo

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.util import convert_path
+from setuptools import find_packages, setup
+
+
+data = {}
+version_path = convert_path("version.py")
+with open(version_path) as version_file:
+    exec(version_file.read(), data)
+
+
+setup(name="pyqt_virtual_keyboard",
+      version=data["VERSION"],
+      description="Virtual keyboard based on PyQt",
+      url="https://github.com/timerke/pyqt_virtual_keyboard",
+      packages=find_packages(),
+      python_requires=">=3.6, <=3.9.13",
+      install_requires=[
+          "PyQt5>=5.8.2",
+      ],
+      package_data={"keyboard": ["backspace.png",
+                                 "keyboard_en.ui",
+                                 "keyboard_en.ui",
+                                 "up-arrow.png"],
+                    },
+      )
