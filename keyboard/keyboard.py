ef543b405c3dd150bc27de6130feccdd5947788a
timerke <timerke@mail.ru>
2024-07-19 16:37:22 +0300

    Changed language translation

--- keyboard/keyboard.py
+++ keyboard/keyboard.py
@@ -1,33 +1,50 @@
-from PyQt5.QtCore import pyqtSlot, QObject
+from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget
 from .keyboard_window import KeyboardWindow
 
 
-class Keyboard(QObject):
+class Keyboard(QMainWindow):
 
     def __init__(self, callback=None) -> None:
         super().__init__()
         self._callback = callback
         self._english: bool = True
         self._init_keyboards()
+        self._change_size()
+
+    def _change_size(self) -> None:
+        new_width = 400
+        new_height = 250
+        self.resize(new_width, new_height)
 
     def _init_keyboards(self) -> None:
-        self._keyboard_en = KeyboardWindow(self._callback, True)
+        # self.setFixedSize(self.size())
+        self.setWindowFlags(Qt.WindowCloseButtonHint)
+        # self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setWindowModality(Qt.ApplicationModal)
+
+        self._keyboard_en: KeyboardWindow = KeyboardWindow(self._callback, True)
         self._keyboard_en.language_changed.connect(self.change_language)
-        self._keyboard_ru = KeyboardWindow(self._callback, False)
+        self._keyboard_ru: KeyboardWindow = KeyboardWindow(self._callback, False)
         self._keyboard_ru.language_changed.connect(self.change_language)
 
-    @pyqtSlot(bool, str)
-    def change_language(self, english: bool, text: str) -> None:
+        layout = QVBoxLayout()
+        layout.addWidget(self._keyboard_en)
+        layout.addWidget(self._keyboard_ru)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    @pyqtSlot(bool, str, bool)
+    def change_language(self, english: bool, text: str, upper: bool) -> None:
         self._english = english
-        old_keyboard, new_board = (self._keyboard_ru, self._keyboard_en) if self._english else\
-            (self._keyboard_en, self._keyboard_ru)
-        new_board.resize(old_keyboard.size())
-        new_board.move(old_keyboard.pos())
+        new_board = self._keyboard_en if self._english else self._keyboard_ru
         new_board.set_text(text)
+        new_board.change_upper_state(upper)
         self.show()
 
     def show(self) -> None:
-        if self._english:
-            self._keyboard_en.show()
-        else:
-            self._keyboard_ru.show()
+        self._keyboard_en.setVisible(self._english)
+        self._keyboard_ru.setVisible(not self._english)
+        super().show()
--- keyboard/keyboard_window.py
+++ keyboard/keyboard_window.py
@@ -2,51 +2,22 @@ import os
 import re
 from typing import List
 from PyQt5 import uic
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QMainWindow, QPushButton
+from PyQt5.QtWidgets import QPushButton, QWidget
 
 
-LOWER = "QPushButton\n""{\n" \
-            "    background-color: qlineargradient(spread:pad, x1:0.521, y1:0.8925, x2:0.504, y2:0.244227, stop:0.00847458 rgba(238, 235, 232, 255), stop:1 rgba(255, 255, 255, 255));\n"" \
-            ""    border: 1px solid gray;\n" \
-            "    border-radius: 2px;\n" \
-            "}\n" \
-            "\n" \
-            "QPushButton:pressed \n" \
-            "{    \n" \
-            "    background-color: rgb(140, 223, 255);\n" \
-            " }"
+class KeyboardWindow(QWidget):
 
-UPPER = "QPushButton\n""{\n" \
-            "    background-color: rgb(0, 162, 255);\n"" \
-            ""    border: 1px solid gray;\n" \
-            "    border-radius: 2px;\n" \
-            "}\n" \
-            "\n" \
-            "QPushButton:pressed \n" \
-            "{    \n" \
-            "    background-color: rgb(140, 223, 255);\n" \
-            " }"
-
-
-class KeyboardWindow(QMainWindow):
-
-    language_changed: pyqtSignal = pyqtSignal(bool, str)
+    language_changed: pyqtSignal = pyqtSignal(bool, str, bool)
 
     def __init__(self, callback=None, english: bool = True) -> None:
         super().__init__()
         self._english: bool = english
-        lang: str = "en" if english else "ru"
-        uic.loadUi(os.path.join("keyboard", f"window_keyboard_{lang}.ui"), self)
-
-        # self.setFixedSize(self.size())
-        self.setWindowFlags(Qt.WindowCloseButtonHint)
-        # self.setWindowFlags(Qt.FramelessWindowHint)
-        self.setWindowModality(Qt.ApplicationModal)
+        lang = "en" if english else "ru"
+        uic.loadUi(os.path.join("keyboard", f"keyboard_{lang}.ui"), self)
 
         self.cb = callback
-        self.upper: bool = False
 
         self._get_buttons()
         self._setup_listener()
@@ -58,10 +29,6 @@ class KeyboardWindow(QMainWindow):
             button.setFont(font)
 
     def _change_size(self) -> None:
-        new_width = 400
-        new_height = 250
-        self.resize(new_width, new_height)
-
         font_size = 14
         font = QFont()
         font.setPointSize(font_size)
@@ -81,6 +48,7 @@ class KeyboardWindow(QMainWindow):
                 self._buttons_letters.append(getattr(self, attr_name))
             elif re.match(r"btn_.*", attr_name) and getattr(self, attr_name) not in self._buttons_special:
                 self._buttons.append(getattr(self, attr_name))
+        self.btn_space.setText(" ")
 
     def _setup_listener(self) -> None:
         for btn in self._buttons_letters:
@@ -95,12 +63,15 @@ class KeyboardWindow(QMainWindow):
         self.btn_backspace.clicked.connect(self.handle_backspace_clicked)
         self.btn_language.clicked.connect(self.change_language)
         self.btn_ok.clicked.connect(self.handle_ok_clicked)
-        self.btn_upper.clicked.connect(self.handle_upper_clicked)
+        self.btn_upper.toggled.connect(self.handle_upper_clicked)
 
     @pyqtSlot()
     def change_language(self) -> None:
-        self.hide()
-        self.language_changed.emit(not self._english, self.edit_text.text())
+        self.language_changed.emit(not self._english, self.edit_text.text(), self.btn_upper.isChecked())
+
+    def change_upper_state(self, upper: bool) -> None:
+        if upper != self.btn_upper.isChecked():
+            self.btn_upper.toggle()
 
     @pyqtSlot()
     def handle_backspace_clicked(self) -> None:
@@ -121,18 +92,12 @@ class KeyboardWindow(QMainWindow):
     def handle_ok_clicked(self) -> None:
         self.hide()
 
-    @pyqtSlot()
-    def handle_upper_clicked(self) -> None:
-        self.upper = not self.upper
-
-        if self.upper:
-            self.btn_upper.setStyleSheet(UPPER)
-
+    @pyqtSlot(bool)
+    def handle_upper_clicked(self, state: bool) -> None:
+        if state:
             for btn in self._buttons_letters:
                 btn.setText(btn.text().upper())
         else:
-            self.btn_upper.setStyleSheet(LOWER)
-
             for btn in self._buttons_letters:
                 btn.setText(btn.text().lower())
 
