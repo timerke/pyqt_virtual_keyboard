7b5d6d839b154aabcdcd1e67e91a31cfed5dca18
timerke <timerke@mail.ru>
2024-07-19 13:43:28 +0300

    Added gitignore, changed keyboard, added Ru and En keyboards

---
+++ gui/keyboard.py
@@ -0,0 +1,33 @@
+from PyQt5.QtCore import pyqtSlot, QObject
+from gui.keyboard_window import KeyboardWindow
+
+
+class Keyboard(QObject):
+
+    def __init__(self, callback=None) -> None:
+        super().__init__()
+        self._callback = callback
+        self._english: bool = True
+        self._init_keyboards()
+
+    def _init_keyboards(self) -> None:
+        self._keyboard_en = KeyboardWindow(self._callback, True)
+        self._keyboard_en.language_changed.connect(self.change_language)
+        self._keyboard_ru = KeyboardWindow(self._callback, False)
+        self._keyboard_ru.language_changed.connect(self.change_language)
+
+    @pyqtSlot(bool, str)
+    def change_language(self, english: bool, text: str) -> None:
+        self._english = english
+        old_keyboard, new_board = (self._keyboard_ru, self._keyboard_en) if self._english else\
+            (self._keyboard_en, self._keyboard_ru)
+        new_board.resize(old_keyboard.size())
+        new_board.move(old_keyboard.pos())
+        new_board.set_text(text)
+        self.show()
+
+    def show(self) -> None:
+        if self._english:
+            self._keyboard_en.show()
+        else:
+            self._keyboard_ru.show()
---
+++ main_window.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLineEdit, QMainWindow, QVBoxLayout, QWidget
+from gui.keyboard import Keyboard
+
+
+class MainWindow(QMainWindow):
+
+	def __init__(self) -> None:
+		super().__init__()
+		self._init_ui()
+
+	def _init_ui(self) -> None:
+		self.kb = Keyboard(self)
+		self.focused_input = None
+
+		self.line_edit: QLineEdit = QLineEdit()
+		self.line_edit.mousePressEvent = self.handle_mouse_press
+
+		layout = QVBoxLayout()
+		layout.addWidget(self.line_edit)
+
+		widget = QWidget()
+		widget.setLayout(layout)
+		self.setCentralWidget(widget)
+
+	def handle_mouse_press(self, e):
+		self.focused_input = self.line_edit
+		self.kb.show()
+
+	def on_key(self, keys):
+		if self.focused_input is not None:
+			self.focused_input.setText(keys)
+
+
+if __name__ == "__main__":
+	app = QApplication(sys.argv)
+	m_window = MainWindow()
+	m_window.show()
+	app.exec_()
